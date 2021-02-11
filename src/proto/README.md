92d93dff4dafd93b5ec539525e679bd57a41865b
Rodrigo Silveira <r3850365@gmail.com>
2021-02-10 18:48:55 -0800

    master  -  Added the greeter.proto and tools to compile it and generate its types

---
+++ src/proto/README.md
@@ -0,0 +1,2 @@
+#Development Notes
+* You need to generate the typescript definitions and update `./index.ts`  whenever you create new proto files.
---
+++ src/proto/index.ts
@@ -0,0 +1,7 @@
+// You need to generate the typescript definitions and update this file
+// whenever you create new proto files.
+import './greeter/greeter_pb';
+import './greeter/greeter_grpc_pb';
+
+export const protoIndex:any = ():void => {
+};
